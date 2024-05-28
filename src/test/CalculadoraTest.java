36f684d235501af6cf0cb6368a47f6aca98485a3
jvrdms <jvrdms@gmail.com>
2024-05-28 21:39:55 +0200

    creada prueba testSuma

--- src/test/CalculadoraTest.java
+++ src/test/CalculadoraTest.java
@@ -1,2 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculadoraTest {
+    @Test
+    void testSuma() {
+        // Arrange
+        Calculadora calculadora = new Calculadora();
+        // Act
+        int resultado = calculadora.suma(3, 2);
+        // Assert
+        assertEquals(5, resultado);
+    }
 }
