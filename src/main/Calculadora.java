4e11da75b6cf84fdabce4494e880bd3fcf2eb254
jvrdms <jvrdms@gmail.com>
2024-05-31 13:12:21 +0200

    Codificada funcionalidad resta

--- src/main/Calculadora.java
+++ src/main/Calculadora.java
@@ -1,5 +1,10 @@
 public class Calculadora {
+
     public static double suma(double a, double b) {
         return a + b;
+    }
+
+    public static double resta(double a, double b) {
+        return a - b;
     }
 }
--- src/test/CalculadoraTest.java
+++ src/test/CalculadoraTest.java
@@ -6,24 +6,24 @@ public class CalculadoraTest {
     @Test
     void testSuma() {
         // Arrange
-        double a = 3.456;
-        double b = 7.89;
-        double resultadoEsperado = 11.346;
+        double a = 3.456d;
+        double b = 7.89d;
+        double resultadoEsperado = 11.346d;
         // Act
         double resultadoObtenido = Calculadora.suma(a, b);
         // Assert
-        assertEquals(resultadoEsperado, resultadoObtenido);
+        assertEquals(resultadoEsperado, resultadoObtenido,3);
     }
 
     @Test
     void testResta() {
         // Arrange
-        double a = 3.456;
-        double b = 7.89;
-        double resultadoEsperado = -4.434;
+        double a = 3.456d;
+        double b = 7.89d;
+        double resultadoEsperado = -4.434d;
         // Act
         double resultadoObtenido = Calculadora.resta(a, b);
         // Assert
-        assertEquals(resultadoEsperado, resultadoObtenido);
+        assertEquals(resultadoEsperado, resultadoObtenido, 3);
     }
 }
