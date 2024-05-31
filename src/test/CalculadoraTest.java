f2b57843fb4a5d5d833d0164002b1780ef90f730
jvrdms <jvrdms@gmail.com>
2024-05-31 12:45:38 +0200

    Definida prueba suma

--- src/test/CalculadoraTest.java
+++ src/test/CalculadoraTest.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculadoraTest {
+    @Test
+    void testSuma() {
+        // Arrange
+        double a = 3.456;
+        double b = 7.89;
+        double resultadoEsperado = 11.346;
+        // Act
+        double resultadoObtenido = Calculadora.suma(a, b);
+        // Assert
+        assertEquals(resultadoEsperado, resultadoObtenido);
+    }
 }
